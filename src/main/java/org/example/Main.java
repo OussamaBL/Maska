c9af617aa268a8ef677a0b075275f46472b6b01d
OussamaBL <oussamaazrour@gmail.com>
2024-10-24 23:49:01 +0100

    Integration des service et repository de membre et fichier jsp de listmembre

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        
         // Retrieve the MemberService bean (Constructor Injection example)
         MemberService memberService = context.getBean(MemberService.class);
 
